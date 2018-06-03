89b29875dfdd06fd531fab69536ebdbe4a4e577d
Zakaria Khan <zakaria_yahhia@hotmail.com>
2018-06-03 17:28:47 +0100

    3rd Sumition
    Service worker is working now.

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ var staticCacheName = 'restaurant-cache-v1';
 var restaurantFileToCache = [
 '/',
 './index.html',
-'./restaurant.html',
+'/restaurant.html',
 './css/styles.css',
 './js/dbhelper.js',
 './js/main.js',
@@ -25,12 +25,17 @@ var restaurantFileToCache = [
 './img/10.jpg'
 ];
 // downloading the files
-self.addEventListener('install', function(event){
-	event.waitUntil(
-	caches.open(staticCacheName).then(function(cache){
-			return cache.addAll(restaurantFileToCache);
-		});
-	);
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  // Perform install steps
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(restaurantFileToCache);
+      })
+  );
 });
 self.addEventListener('fetch', function(event) {
   event.respondWith(
@@ -40,85 +45,9 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(staticCacheName)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+        return fetch(event.request);
+      }
+    )
   );
 });
-// https://developers.google.com/web/fundamentals/primers/service-workers/
-/* fetching the files from the cache
-self.addEventListener('fetch', function(event){
-	event.respondWidth(
-		// This line of code fetches the files fromt cache
-		caches.match(event.request).then(function(response){
-			if(response){
-				return response;
-			}
-			// Fetches the requested files from the network then caches in caches file
-			return fetch(event.request).then(function(response){
-				return caches.open(staticCacheName).then(function(cache){
-					cache.put(event.request.url, response.clone);
-					return response;
-				});
-			});
-		});
-	);
-});
-// update the old version of cache to new version
-self.addEventListener('activate', function(event){
-	var cacheWhitelist = [staticCacheName];
-	event.waitUntil(
-		caches.key().then(function(cacheNames){
-			return Promise.all(
-				cacheNames.map(function(cacheName){
-					if(cacheWhitelist.indexOf(cacheName)=== -1){
-						return caches.delete(cacheName)
-					}
-				});
-			);
-		});
-	);
-});
-*/
+
