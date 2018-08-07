07eec4cd2298b57fea245ad4ba7414ea8c0c0e05
Zakaria Khan <zakaria_yahhia@hotmail.com>
2018-08-07 10:08:39 +0100

    Add files via upload

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,3 +1,4 @@
+
 /**
  * Common database helper functions.
  */
@@ -8,33 +9,32 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+
+    fetch(DBHelper.DATABASE_URL)
+    .then(response =>{
+      response.json()
+      .then(restaurants => console.log(restaurants));
+        callback(restaurants);
+    })
+    .catch(error =>{
+      callback(`Request failed${error}`);
+    })
   }
 
+
+/// everything above that needs to be changed ////
   /**
    * Fetch a restaurant by its ID.
    */
-  static fetchRestaurantById(id, callback) {
+     static fetchRestaurantById(id, callback) {
     // fetch all restaurants with proper error handling.
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
@@ -50,9 +50,6 @@ class DBHelper {
     });
   }
 
-  /**
-   * Fetch restaurants by a cuisine type with proper error handling.
-   */
   static fetchRestaurantByCuisine(cuisine, callback) {
     // Fetch all restaurants  with proper error handling
     DBHelper.fetchRestaurants((error, restaurants) => {
